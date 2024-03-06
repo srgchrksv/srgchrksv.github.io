8cc583cb407699063af73c6e183411562b77bf38
Sergei Cherkasov <155839413+srgchrksv@users.noreply.github.com>
2024-03-06 13:19:37 +0000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://srgchrksv.github.io',
+  site: 'https://srgchrksv.github.io',
+  integrations: [sitemap()]
 });
